decd0e228925500d6da4a66036e5005881f47e41
victorhong1988 <victorhong1988@gmail.com>
2016-11-04 15:51:19 -0400

    Initial Commit

---
+++ Pig Latin/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pig Latin
+//
+//  Created by Victor Hong on 04/11/2016.
+//  Copyright © 2016 Victor Hong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
